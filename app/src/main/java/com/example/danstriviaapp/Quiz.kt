6b1da622f6f758af5c9e980b88f092776e49c671
Daniel Dineen <dannydineen2002@gmail.com>
2023-08-24 16:03:08 +0100

    The quiz activity now displays a random question from the question pool, and the associated options with that question. THe buttons currently don't let you pick an answer yet.

--- app/src/main/java/com/example/danstriviaapp/Quiz.kt
+++ app/src/main/java/com/example/danstriviaapp/Quiz.kt
@@ -4,6 +4,10 @@ import Constants.GeneralKnowledgeConstants.returnGK1
 import Constants.Question
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_quiz.*
+import java.util.Collections.shuffle
+
+var index = 0
 
 class Quiz : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +25,25 @@ class Quiz : AppCompatActivity() {
 
         if (quizName == "General Knowledge 1"){
             questionList = returnGK1(this)
-            val questionText = questionList[0].questionText
-            println("Quiz Object: "+questionText)
+            displayOptions(questionList)
         }
+    }
+
+    fun displayOptions(questionList:ArrayList<Question>){
+        val randomQuestions = ArrayList(questionList).apply { shuffle() }
+        val question = randomQuestions[index]
+        val questionText = question.questionText
+        val options = question.options
+        val correctOption = question.correctAnswer
+
+        //Change the question to display the current question
+        tv_question.text = questionText
+
+        //Change the option buttons to the different answer options
+        bt_option_one.text = options[0]
+        bt_option_two.text = options[1]
+        bt_option_three.text = options[2]
+        bt_option_four.text = options[3]
+
     }
 }
