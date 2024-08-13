cdf84a00b669343f27a14ebeea26d96f2543b533
DanD0307 <dannydineen2002@gmail.com>
2024-08-13 12:06:12 +0100

    Added some functionality to the TypingQuiz.kt class. It still doesn't work completely but it displays the question and checks if the answer is correct or not. No progression to the next question though that is the next step. Need to add a continue button, track progress, display final score and allow the user to restart the quiz at the end.

--- app/src/main/java/com/example/danstriviaapp/TypingQuiz.kt
+++ app/src/main/java/com/example/danstriviaapp/TypingQuiz.kt
@@ -2,11 +2,89 @@ package com.example.danstriviaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import constants.GeneralKnowledgeConstants
+import constants.Question
+import kotlinx.android.synthetic.main.activity_typing_quiz.*
 
 class TypingQuiz : AppCompatActivity() {
+
+    var index = 0
+    var correctOption = 0
+    var questionOverFlag = false
+    var endOfQuizFlag = false
+    var hundredPercentFlag = false
+    var inSubQuiz = false
+    var questionList = arrayListOf<Question>()
+    var incorrectQuestionList = arrayListOf<Question>()
+
+    var quizName = ""
+    var primaryColour = 0
+    val GREEN_COLOUR = "#00FF00"
+    val RED_COLOUR = "#FF0000"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_typing_quiz)
 
+        //This gets the quizName (what quiz we're currently in, from the code)
+        quizName = intent.getStringExtra("quizName").toString()
+        println("THE QUIZNAME IS: " + quizName)
+
+        initialiseButtonListeners()
+        startQuiz(quizName)
+
     }
+
+    private fun initialiseButtonListeners() {
+        //First Button
+        bt_submit.setOnClickListener {
+            //If user clicks submit we check to see if the answer is correct, remove the text from the ET and display green or red and show correct answer if applicable
+            if(et_answer.text.toString() != ""){
+                checkAnswer()
+            }
+
+        }
+    }
+
+    private fun startQuiz(quizName:String){
+        if (quizName == "Europe Type the Capital") {
+            questionList = GeneralKnowledgeConstants.returnTypedQuiz(this,"capitalcities/europeTypeTheCapital.txt")
+        }
+
+        questionList = ArrayList(questionList).apply { shuffle() }
+        tv_quiz_progress.text = "0/${questionList.size}"
+        progressBar.max = questionList.size
+        displayQuestion()
+    }
+
+    private fun displayQuestion(){
+
+        val question = questionList[index]
+        tv_question.text = question.questionText
+
+    }
+
+    private fun checkAnswer(){
+        //TODO I need a way of consistently making the answers lowercase so it compares properly in the if statement, but also displaying the answer in true form
+        val usersAnswer = et_answer.text.toString().lowercase()
+        val question = questionList[index]
+        val displayAnswer = question.options[0]
+        val answers = question.options.map { it.lowercase() }
+
+        if(usersAnswer in answers){
+
+            println("CORRECT,ITS RIGHT")
+            tv_question.text = "Correct!"
+
+        }
+        else{
+            tv_question.text = "Incorrect. The correct answer is: "+displayAnswer
+
+        }
+
+
+
+    }
+
+
 }
--- app/src/main/java/constants/GeneralKnowledgeConstants.kt
+++ app/src/main/java/constants/GeneralKnowledgeConstants.kt
@@ -18,7 +18,7 @@ object GeneralKnowledgeConstants {
         val list = returnArrayList(IS)
         return list
     }
-
+    //-----FUNCTIONALITY FOR RETURNING A QUIZ WITH MULTIPLE OPTIONS-----
     fun returnQuiz(context: Context, quizPath:String):ArrayList<Question> {
         val manager: AssetManager = context.getAssets()
         val IS : InputStream = manager.open(quizPath)
@@ -39,7 +39,34 @@ object GeneralKnowledgeConstants {
             list.add(question)
 
         }
-
         return list
     }
+
+
+    //-----FUNCTIONALITY FOR RETURNING A QUIZ WITH ONE ANSWER OTHERWISE KNOWN AS A TYPED QUIZ-----
+    //For MCQ I use data class called a question to store the question text and an array of the options
+    //I reuse the data class for typed quizzes but instead of there being options they are alternative answers
+    //For example different spellings.
+    //However, some questions might only have one interpretable answer so I need a seperate function to deal with variablity in 'option' length
+
+    fun returnTypedQuiz(context: Context, quizPath:String):ArrayList<Question> {
+        val manager: AssetManager = context.getAssets()
+        val IS : InputStream = manager.open(quizPath)
+        val list = returnArrayListTyped(IS)
+        return list
+    }
+
+    private fun returnArrayListTyped(IS: InputStream): ArrayList<Question> {
+        val list: ArrayList<Question> = arrayListOf()
+
+        IS.bufferedReader().forEachLine {
+            val array = it.split("#").toTypedArray()
+            val text = array[0]
+            val options = array.slice(1 until array.size) //Get everything after the first element which is the question
+            val question = Question(text, options)
+            list.add(question)
+        }
+        return list
+    }
+
 }
