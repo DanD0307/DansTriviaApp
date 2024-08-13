e4f787215383c1f24f7415748b7e922412eaf14e
DanD0307 <dannydineen2002@gmail.com>
2024-08-13 22:05:28 +0100

    I've implemented progression into the TypingQuiz.kt with a continue button being displayed after the user enters their answer. Next step is to implement the end screen which is going to be different from how I've done it with the regular multiple choice quiz. Instead of dynamically modifying the layout I want to use a viewSwitcher to essentially switch to a new layout for the end screen.

--- app/src/main/java/com/example/danstriviaapp/TypingQuiz.kt
+++ app/src/main/java/com/example/danstriviaapp/TypingQuiz.kt
@@ -2,14 +2,17 @@ package com.example.danstriviaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import constants.GeneralKnowledgeConstants
 import constants.Question
 import kotlinx.android.synthetic.main.activity_typing_quiz.*
+import kotlinx.android.synthetic.main.activity_typing_quiz.bt_continue
+import kotlinx.android.synthetic.main.activity_typing_quiz.tv_question
+import kotlinx.android.synthetic.main.activity_typing_quiz.tv_quiz_progress
 
 class TypingQuiz : AppCompatActivity() {
 
     var index = 0
-    var correctOption = 0
     var questionOverFlag = false
     var endOfQuizFlag = false
     var hundredPercentFlag = false
@@ -36,13 +39,31 @@ class TypingQuiz : AppCompatActivity() {
     }
 
     private fun initialiseButtonListeners() {
-        //First Button
+        //Submit Button
         bt_submit.setOnClickListener {
-            //If user clicks submit we check to see if the answer is correct, remove the text from the ET and display green or red and show correct answer if applicable
-            if(et_answer.text.toString() != ""){
-                checkAnswer()
+
+            if(questionOverFlag){
+                bt_submit.text = "SUBMIT"
+                questionOverFlag = false
+                et_answer.setVisibility(View.VISIBLE)
+                et_answer.requestFocus() //This makes the user ready to type in the next answer as soon as the ET reappears
+                nextQuestion()
             }
 
+            //If user clicks submit we check to see if the answer is correct, remove the text from the ET and display green or red and show correct answer if applicable
+            else if(et_answer.text.toString() != ""){
+                checkAnswer()
+                et_answer.setText("")
+                et_answer.setVisibility(View.INVISIBLE)
+            }
+        }
+
+        //Continue Button MIGHT BE DISCONTINUED
+        bt_continue.setOnClickListener {
+            //Show the next question and then make the button invisible again
+            bt_continue.setVisibility(View.GONE);
+            et_answer.setVisibility(View.VISIBLE)
+            nextQuestion()
         }
     }
 
@@ -58,33 +79,46 @@ class TypingQuiz : AppCompatActivity() {
     }
 
     private fun displayQuestion(){
-
         val question = questionList[index]
         tv_question.text = question.questionText
+    }
 
+    private fun nextQuestion(){
+        index += 1
+        progressBar.progress = index
+        tv_quiz_progress.text = "$index/${questionList.size}"
+
+        if(index == questionList.size) {
+            endQuiz()
+            return
+        }
+        displayQuestion()
     }
 
     private fun checkAnswer(){
-        //TODO I need a way of consistently making the answers lowercase so it compares properly in the if statement, but also displaying the answer in true form
-        val usersAnswer = et_answer.text.toString().lowercase()
+        val usersAnswer = et_answer.text.toString().trim().lowercase()//Trim removes whitespace at start and end of answer
         val question = questionList[index]
         val displayAnswer = question.options[0]
         val answers = question.options.map { it.lowercase() }
 
+        //If answer is correct:
         if(usersAnswer in answers){
-
             println("CORRECT,ITS RIGHT")
             tv_question.text = "Correct!"
+
 
         }
         else{
             tv_question.text = "Incorrect. The correct answer is: "+displayAnswer
-
+            incorrectQuestionList.add(question)
         }
 
-
-
+        questionOverFlag = true
+        bt_submit.text = "CONTINUE"
     }
 
+    private fun endQuiz(){
+        tv_question.text = "QUIZ IS OVER"
+    }
 
 }
